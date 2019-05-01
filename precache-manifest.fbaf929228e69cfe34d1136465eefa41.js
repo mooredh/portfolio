3eeedc144f0c16b79bcf961c90f5af39dd9ecb2d
Moore Dagogo Hart <mooredh99@gmail.com>
2019-05-01 23:20:07 +0100

    Updates

---
+++ precache-manifest.fbaf929228e69cfe34d1136465eefa41.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "377ba7a9ae1e31c493ae1b2ee1c87224",
+    "url": "/static/media/dealin.377ba7a9.png"
+  },
+  {
+    "revision": "7b103948f76ec614b9df",
+    "url": "/static/css/main.8c552a32.chunk.css"
+  },
+  {
+    "revision": "08c5d404c09251abaa14",
+    "url": "/static/js/1.08c5d404.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a51c2d180ca6c8553e8f2920b50d6468",
+    "url": "/static/media/one.a51c2d18.jpg"
+  },
+  {
+    "revision": "243d7c2fed9d2418d97e613cca97adef",
+    "url": "/static/media/two.243d7c2f.jpg"
+  },
+  {
+    "revision": "1e3e2159a4709b6a137c3d1a7525dafa",
+    "url": "/static/media/color-match.1e3e2159.png"
+  },
+  {
+    "revision": "464abd2c93593a4db6e3964ba5857873",
+    "url": "/static/media/elixir.464abd2c.png"
+  },
+  {
+    "revision": "f80e450734e4b3f0e25cc0c5fec3fb91",
+    "url": "/static/media/resume.f80e4507.png"
+  },
+  {
+    "revision": "7b103948f76ec614b9df",
+    "url": "/static/js/main.7b103948.chunk.js"
+  },
+  {
+    "revision": "e7565e970802a3fad6c9f42ed096b352",
+    "url": "/static/media/bulc.e7565e97.png"
+  },
+  {
+    "revision": "b06ad7b83e55d7b3599a21635ab88644",
+    "url": "/static/media/Sansation-Regular.b06ad7b8.ttf"
+  },
+  {
+    "revision": "37c961d1db011f138962a4c60f356346",
+    "url": "/static/media/Sansation-Bold.37c961d1.ttf"
+  },
+  {
+    "revision": "4e2cc6069f7f1d12ad99cf4b0182d4bc",
+    "url": "/static/media/Sansation-BoldItalic.4e2cc606.ttf"
+  },
+  {
+    "revision": "816fd44e8b465216b1a3b33fb5700f39",
+    "url": "/static/media/Sansation-Italic.816fd44e.ttf"
+  },
+  {
+    "revision": "76d89f21a030d8bb911a341291e5fb7c",
+    "url": "/static/media/Sansation-Light.76d89f21.ttf"
+  },
+  {
+    "revision": "15aa3af86b7c4422d8010c93d18a080c",
+    "url": "/static/media/Sansation-LightItalic.15aa3af8.ttf"
+  },
+  {
+    "revision": "fd2f2c57f4c5dc84aafe6c69b2a72b64",
+    "url": "/index.html"
+  }
+];
